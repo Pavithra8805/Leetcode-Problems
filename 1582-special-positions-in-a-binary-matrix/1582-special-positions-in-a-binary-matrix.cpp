479d9d4d4f6a40300bb273db324d58c522689e80
Pavithra8805 <141915927+Pavithra8805@users.noreply.github.com>
2026-03-05 14:43:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
-        int rows = mat.size();
-        int cols = mat[0].size();
-
-        vector<int> rowCount(rows, 0);
-        vector<int> colsCount(cols, 0);
-
-        for(int i=0; i<rows; i++) {
-            for(int j=0; j<cols; j++) {
-                if(mat[i][j] == 1) {
-                    rowCount[i]++;
-                    colsCount[j]++;
+        int row=mat.size();
+        int col=mat[0].size();
+        vector <int> rowcount(row,0);
+        vector <int> colcount(col,0);
+        int count=0;
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(mat[i][j]==1)
+                {
+                    rowcount[i]++;
+                    colcount[j]++;
                 }
             }
         }
-
-        int count = 0;
-        for(int i=0; i<rows; i++) {
-            for(int j=0; j<cols; j++) {
-                if(mat[i][j] == 1 && rowCount[i] == 1 && colsCount[j] == 1) {
+        for(int i =0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(mat[i][j]==1 && rowcount[i]==1 && colcount[j]==1)
+                {
                     count++;
                 }
             }
         }
         return count;
+        
     }
 };
