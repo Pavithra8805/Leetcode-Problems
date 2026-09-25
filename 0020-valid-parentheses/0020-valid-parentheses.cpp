dcb99cfd3c123ab042bc75cda5dea7a431fb360b
Pavithra8805 <141915927+Pavithra8805@users.noreply.github.com>
2026-09-25 17:15:33 +0530

    Time: 3 ms (9.29%), Space: 10 MB (6.15%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     bool isValid(string s) {
         if (s.length() % 2 != 0) return false;
-
         stack<char> st;
-
         for (char c : s) {
             if (c == '(') {
                 st.push(')');
