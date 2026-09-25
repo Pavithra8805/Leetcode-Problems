ce3e4d7a562a44ec1c862afd95a8e39498d203db
Pavithra8805 <141915927+Pavithra8805@users.noreply.github.com>
2026-09-25 17:47:50 +0530

    Time: 0 ms (100%), Space: 12.5 MB (79.57%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,39 +1,21 @@
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& nums1,
-                                   vector<int>& nums2) {
-
-        unordered_map<int,int> mp;
-
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> next_greater;
         stack<int> st;
 
-        for(int i=nums2.size()-1;i>=0;i--) {
-
-            while(!st.empty() &&
-                  st.top()<=nums2[i]) {
-
+        for (int num : nums2) {
+            while (!st.empty() && st.top() < num) {
+                next_greater[st.top()] = num;
                 st.pop();
             }
-
-            // No greater element
-            if(st.empty()) {
-
-                mp[nums2[i]]=-1;
-            }
-
-            else {
-
-                mp[nums2[i]]=st.top();
-            }
-
-            st.push(nums2[i]);
+            st.push(num);
         }
 
         vector<int> ans;
-
-        for(int num:nums1) {
-
-            ans.push_back(mp[num]);
+        ans.reserve(nums1.size());
+        for (int num : nums1) {
+            ans.push_back(next_greater.count(num) ? next_greater[num] : -1);
         }
 
         return ans;
